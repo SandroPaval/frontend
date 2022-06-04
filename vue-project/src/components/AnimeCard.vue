4f943212a14f9a14fef5b740fa3ffdda251bc38a
Paval, Sandro <sandro.paval@student.fhws.de>
2022-06-04 23:07:32 +0200

    addes shadow for cards

--- vue-project/src/components/AnimeCard.vue
+++ vue-project/src/components/AnimeCard.vue
@@ -35,6 +35,7 @@ margin-top: 15px;
 background: rgb(17, 16, 16);
 border-radius: 20px;
 position: relative;
+box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
 }
 
 .image{
